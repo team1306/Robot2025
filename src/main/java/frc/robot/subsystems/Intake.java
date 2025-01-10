08e76caa084d27fdbd47c0cee9a8d59992b751de
Bradley Chumanov <bradley.chumanov@gmail.com>
2025-01-09 20:21:25 -0600

    fix compile error

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -10,16 +10,14 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import static frc.robot.Constants.*;
 
-import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkRelativeEncoder;
 
 
 public class Intake extends SubsystemBase{
 
-    private final SparkMax motor = MotorUtil.initSparkMax(INTAKE_ID, MotorType.kBrushless, IdleMode.kBrake);
+    private final SparkMax motor = MotorUtil.initSparkMax(INTAKE_ID, IdleMode.kBrake);
     private final NeoGroup neoGroup = new NeoGroup(new NeoGroup.NeoData(motor, false));
     private final RelativeEncoder encoder = motor.getEncoder();
     
