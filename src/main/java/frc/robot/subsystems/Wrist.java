0a9dbc7d3672cfaacf4599af59a30b4b62562146
emijly <emily2004.yao@gmail.com>
2025-01-09 18:47:17 -0600

    Created files for arm, elevator, and wrist subsystems

---
+++ src/main/java/frc/robot/subsystems/Wrist.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Wrist extends SubsystemBase {
+    
+    // control falcon 500
+    // know theres a gear ratio
+    // have pid constants
+    // has rev absolute analog encoder to know position
+    // be able to set angle in degrees or radians
+    public Wrist() {
+
+    }
+}
