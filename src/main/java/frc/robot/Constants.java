3b1019c8c451e34f12d71e7dd061425ce8ae33fb
emijly <emily2004.yao@gmail.com>
2025-01-11 11:36:12 -0600

    started working on elevator

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,6 +16,8 @@ public final class Constants {
     public static final int           WRIST_LEFT_MOTOR_ID            = 12334;
     public static final int           WRIST_RIGHT_MOTOR_ID           = 123345;
     public static final int           WRIST_MOTOR_ID                 = -1;
+    public static final int           ELEVATOR_LEFT_MOTOR_ID         = -1;
+    public static final int           ELEVATOR_RIGHT_MOTOR_ID        = -1;
 
     public static final int           NEO_COUNTS_PER_REVOLUTION      = 42;
     public static final int           NEO_CURRENT_LIMIT_AMPS         = 60; // motor would reach safety limit after ~70s of stalling at 60A
--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,9 +1,26 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.util.MotorUtil;
 
 public class Elevator extends SubsystemBase {
     
+    // go to and hold positions (thrifty elevator) [PID and Feedforward]
+
+    private final double PID_P = 1, PID_I = 0, PID_D = 0;
+    private PIDController pid = new PIDController(PID_P, PID_I, PID_D);
+
+    private final double FF_S = 0, FF_G = 0, FF_V = 0;
+    private ElevatorFeedforward feedforward = new ElevatorFeedforward(FF_S, FF_G, FF_V);
+
+    private final SparkMax leftMotor = MotorUtil.initSparkMax(Constants.ELEVATOR_LEFT_MOTOR_ID, IdleMode.kBrake),
+                           rightMotor = MotorUtil.initSparkMax(Constants.ELEVATOR_RIGHT_MOTOR_ID, IdleMode.kBrake);
 
     public Elevator() {
 
