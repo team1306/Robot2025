5fe285acde546431bffabddaac439046cb377e6b
Connor Mulligan <cgmulligan33@gmail.com>
2025-01-12 13:38:34 -0600

    changed elevator command and enum to use Distance insetad of inches

--- src/main/java/frc/robot/commands/elevator/ElevatorSetpoints.java
+++ src/main/java/frc/robot/commands/elevator/ElevatorSetpoints.java
@@ -1,18 +1,21 @@
 package frc.robot.commands.elevator;
 
+import static edu.wpi.first.units.Units.Inches;
+
+import edu.wpi.first.units.measure.Distance;
 import frc.robot.Constants;
 
 public enum ElevatorSetpoints {
     
-    CORAL_L1(18 - Constants.ELEVATOR_STARTING_HEIGHT),
-    CORAL_L2(31.875 - Constants.ELEVATOR_STARTING_HEIGHT),
-    CORAL_L3(47.625 - Constants.ELEVATOR_STARTING_HEIGHT),
-    CORAL_L4(72 - Constants.ELEVATOR_STARTING_HEIGHT);
+    CORAL_L1(Inches.of(18 - Constants.ELEVATOR_STARTING_HEIGHT)),
+    CORAL_L2(Inches.of(31.875 - Constants.ELEVATOR_STARTING_HEIGHT)),
+    CORAL_L3(Inches.of(47.625 - Constants.ELEVATOR_STARTING_HEIGHT)),
+    CORAL_L4(Inches.of(72 - Constants.ELEVATOR_STARTING_HEIGHT));
 
 
-    public final double height; //inches
+    public final Distance height; //inches
 
-    private ElevatorSetpoints(double inches) {
-        this.height = inches;
+    private ElevatorSetpoints(Distance height) {
+        this.height = height;
     }
 }
--- src/main/java/frc/robot/commands/elevator/MoveElevatorToSetpointCommand.java
+++ src/main/java/frc/robot/commands/elevator/MoveElevatorToSetpointCommand.java
@@ -1,34 +1,27 @@
 package frc.robot.commands.elevator;
 
-import static edu.wpi.first.units.Units.Inches;
-
+import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Elevator;
 
 public class MoveElevatorToSetpointCommand extends Command {
     private Elevator elevator;
     
-    private double setpointInches;
-
-    public MoveElevatorToSetpointCommand(Elevator elevator, double setpointInches) {
-        this.setpointInches = setpointInches;
-        this.elevator = elevator;
-        addRequirements(elevator);
-    }
-
+    private Distance targetHeight;
+    
     public MoveElevatorToSetpointCommand(Elevator elevator, ElevatorSetpoints elevatorSetpoint) {
-        this.setpointInches = elevatorSetpoint.height;
+        this.targetHeight = elevatorSetpoint.height;
         this.elevator = elevator;
         addRequirements(elevator);
     }
 
     @Override
     public void execute() {
-        elevator.setTargetHeight(Inches.of(setpointInches));
+        elevator.setTargetHeight(targetHeight);
     }
 
     @Override
     public boolean isFinished() {
-        return elevator.getCurrentHeight().in(Inches) == setpointInches;
+        return elevator.getCurrentHeight() == targetHeight;
     }
 }
