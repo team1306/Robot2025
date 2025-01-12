92079d42ce77b5308b3a021480686eec588237f1
Connor Mulligan <cgmulligan33@gmail.com>
2025-01-12 15:26:01 -0600

    small fix

--- src/main/java/frc/robot/commands/wrist/ManualWristControl.java
+++ src/main/java/frc/robot/commands/wrist/ManualWristControl.java
@@ -22,6 +22,6 @@ public class ManualWristControl extends Command {
     public void execute() {
         final double SPEED = 0.01; //some random speed factor
 
-        wrist.setTargetRotation(wrist.getTargetRotation().plus(Rotation2d.fromRadians(doubleSupplier.getAsDouble() * SPEED)));
+        wrist.setTargetAngle(wrist.getTargetAngle().plus(Rotation2d.fromRadians(doubleSupplier.getAsDouble() * SPEED)));
     }
 }
