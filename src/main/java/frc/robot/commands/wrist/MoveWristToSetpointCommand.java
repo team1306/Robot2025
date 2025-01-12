b696e48fe241ccc00fd00c444cba5c122126034d
Connor Mulligan <cgmulligan33@gmail.com>
2025-01-12 14:02:58 -0600

    remove unused imports

--- src/main/java/frc/robot/commands/wrist/MoveWristToSetpointCommand.java
+++ src/main/java/frc/robot/commands/wrist/MoveWristToSetpointCommand.java
@@ -1,7 +1,5 @@
 package frc.robot.commands.wrist;
 
-import java.util.function.BooleanSupplier;
-
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Wrist;
