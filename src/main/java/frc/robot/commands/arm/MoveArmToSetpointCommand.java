4f7de97635eeaac7fa78180e817cd69da97d137e
Connor Mulligan <cgmulligan33@gmail.com>
2025-01-12 15:03:14 -0600

    added move arm to setpoint command

---
+++ src/main/java/frc/robot/commands/arm/MoveArmToSetpointCommand.java
@@ -0,0 +1,24 @@
+package frc.robot.commands.arm;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Arm;
+
+public class MoveArmToSetpointCommand extends Command {
+    
+    private Arm arm;
+    private final Rotation2d targetRotation;
+
+
+    public MoveArmToSetpointCommand(Arm arm, ArmSetpoints armSetpoint) {
+        this.arm = arm;
+        this.targetRotation = armSetpoint.rotation;
+
+        addRequirements(arm);
+    }
+
+    @Override
+    public void execute() {
+        arm.setTargetRotation(targetRotation);
+    }
+}
