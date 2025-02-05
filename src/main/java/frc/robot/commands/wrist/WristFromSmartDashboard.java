639b194f05aae6cae7b4ca4cf3b8926d6995df93
ElectricTurtle32 <samuelnathanlewis@gmail.com>
2025-02-04 18:57:26 -0600

    Added smartdashboard control to a comand for the wrist

---
+++ src/main/java/frc/robot/commands/wrist/WristFromSmartDashboard.java
@@ -0,0 +1,33 @@
+package frc.robot.commands.wrist;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Wrist;
+import frc.robot.util.Dashboard.DashboardHelpers;
+import frc.robot.util.Dashboard.GetValue;
+
+public class WristFromSmartDashboard extends Command {
+
+   
+    private final Wrist wrist;
+    @GetValue
+    private Rotation2d targetRotation;
+    
+    /**
+     * Sets the wrist angle
+     * @param wristSetpoint setpoint for the wrist to go to
+     */
+    public WristFromSmartDashboard(Wrist wrist) {
+        
+        this.wrist = wrist;
+        DashboardHelpers.addUpdateClass(this);
+        addRequirements(wrist);
+    }
+
+    @Override
+    public void execute() {
+        wrist.setTargetAngle(targetRotation);
+    }
+
+   
+}
