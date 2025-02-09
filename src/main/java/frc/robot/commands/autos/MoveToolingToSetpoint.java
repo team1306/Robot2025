c1c14259f40709642e8c2a668f8a6630fbe195b8
Connor Mulligan <cgmulligan33@gmail.com>
2025-02-09 16:12:08 -0600

    added safe movetotoolingsetpoint

--- src/main/java/frc/robot/commands/autos/MoveToolingToSetpoint.java
+++ src/main/java/frc/robot/commands/autos/MoveToolingToSetpoint.java
@@ -1,5 +1,7 @@
 package frc.robot.commands.autos;
 
+import static edu.wpi.first.units.Units.Inches;
+
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.commands.arm.ArmSetpoint;
 import frc.robot.commands.arm.MoveArmToSetpoint;
@@ -15,12 +17,32 @@ public class MoveToolingToSetpoint extends ParallelCommandGroup {
     /**
      * Move elevator, arm, and wrist to setpoints in a parallel command group
      */
+    public MoveToolingToSetpoint(Elevator elevator, Arm arm, Wrist wrist, ElevatorSetpoint elevatorSetpoint, ArmSetpoint armSetpoint, WristSetpoint wristSetpoint, boolean overrideSafetyMode) {
+        if (overrideSafetyMode)
+            addCommands(
+                new MoveElevatorToSetpoint(elevator, elevatorSetpoint),
+                new MoveArmToSetpoint(arm, armSetpoint),
+                new MoveWristToSetpoint(wrist, wristSetpoint)
+            );
+        else 
+            addCommands(
+                new MoveElevatorToSetpoint(elevator, elevatorSetpoint),
+                new StartEventCommand(
+                    new MoveArmToSetpoint(arm, armSetpoint),
+                    () -> Math.abs(wrist.getCurrentAngle().getDegrees()) > 30 || armSetpoint.getAngle().getDegrees() > 70
+                ),
+                new StartEventCommand( //only move wrist when arm is between threshold degrees/elevator is above threshold
+                    new MoveWristToSetpoint(wrist, wristSetpoint),
+                    () -> elevator.getCurrentHeight().in(Inches) > 10 || arm.getCurrentAngle().getDegrees() > -40,
+                    () -> arm.getCurrentAngle().getDegrees() < 80
+                )
+            );
+    }
+    /**
+     * Move elevator, arm, and wrist to setpoints in a parallel command group
+     */
     public MoveToolingToSetpoint(Elevator elevator, Arm arm, Wrist wrist, ElevatorSetpoint elevatorSetpoint, ArmSetpoint armSetpoint, WristSetpoint wristSetpoint) {
-        addCommands(
-            new MoveElevatorToSetpoint(elevator, elevatorSetpoint),
-            new MoveArmToSetpoint(arm, armSetpoint),
-            new MoveWristToSetpoint(wrist, wristSetpoint)
-        );
+        this(elevator, arm, wrist, elevatorSetpoint, armSetpoint, wristSetpoint, false);
     }
 
 
