c5e7c4ef89fe0fbe1ba20e5c5e1b0ec4838ea919
Bradley Chumanov <bradley.chumanov@gmail.com>
2025-02-13 18:59:21 -0600

    swerve output

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -38,6 +38,7 @@ import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -131,6 +132,7 @@ public class SwerveSubsystem extends SubsystemBase {
                 module.setAnglePIDF(new PIDFConfig(kP, kI, kD));
             }
         }
+        SmartDashboard.putNumber("Power", swerveDrive.getModules()[3].getAngleMotor().getAppliedOutput());
     }
 
     public void addVisionMeasurement(){
