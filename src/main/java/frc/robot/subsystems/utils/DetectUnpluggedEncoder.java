ecbcc150012f72531419b757f762bee745ca77cd
Connor Mulligan <cgmulligan33@gmail.com>
2025-03-11 19:55:53 -0500

    detect encoder unplugged added to arm and wrist (should work but vscode wouldn't work so idk)

---
+++ src/main/java/frc/robot/subsystems/utils/DetectUnpluggedEncoder.java
@@ -0,0 +1,37 @@
+package frc.robot.subsystems.utils;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj.Timer;
+
+public class DetectUnpluggedEncoder {
+
+    private final double timeThresholdSeconds = 2.0;
+
+    private DoubleSupplier encoderSupplier;
+    private DoubleSupplier targetSupplier;
+    private Timer timer = new Timer();
+
+    public DetectUnpluggedEncoder (DoubleSupplier encoderSupplier, DoubleSupplier targetSupplier) {
+        this.encoderSupplier = encoderSupplier;
+        this.targetSupplier = targetSupplier;
+        timer.reset();
+    }
+
+    /**
+     * Run each loop to detect if encoder is unplugged
+     * @return true if encoder is unplugged
+     */
+    public boolean update() {
+        if (targetSupplier.getAsDouble() == 0 || encoderSupplier.getAsDouble() != 0) {
+            timer.reset();
+            return false;
+        }
+        
+        if (timer.get() > timeThresholdSeconds) {
+            return true;
+        }
+
+        return false;
+    }    
+}
