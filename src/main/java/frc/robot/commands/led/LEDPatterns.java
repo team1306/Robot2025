ee54de81f95195277e48b3a7ef987cdcaf3da88b
ElectricTurtle32 <samuelnathanlewis@gmail.com>
2025-03-13 19:31:35 -0500

    added sesziuse mode :)

--- src/main/java/frc/robot/commands/led/LEDPatterns.java
+++ src/main/java/frc/robot/commands/led/LEDPatterns.java
@@ -3,9 +3,14 @@ package frc.robot.commands.led;
 import static edu.wpi.first.units.Units.Inches;
 import static edu.wpi.first.units.Units.InchesPerSecond;
 import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.Percent;
+import static edu.wpi.first.units.Units.Second;
+
+import java.util.Map;
 import java.util.function.DoubleSupplier;
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj.LEDPattern;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.Elevator;
@@ -34,4 +39,16 @@ public class LEDPatterns extends Command {
 
         return Commands.run(() -> subsystem.applyPattern(heightDisplay), subsystem);
     }
+    public static Command seizureMode(LEDSubsystem subsystem){
+        Map<Double, Color> maskSteps = (Map<Double, Color>) Map.of(0.0, Color.kWhite, 0.5, Color.kBlack);
+        final LEDPattern rainbow = LEDPattern.rainbow(255, 75);
+        final LEDPattern base = rainbow.scrollAtAbsoluteSpeed(InchesPerSecond.of(40), Meters.of(1.0/60));
+      
+        LEDPattern mask = LEDPattern.steps(maskSteps).scrollAtRelativeSpeed(Percent.per(Second).of(25));
+
+        final LEDPattern heightDisplay = base.mask(mask);
+
+        return Commands.run(() -> subsystem.applyPattern(heightDisplay), subsystem);
+    }
+    
 }
