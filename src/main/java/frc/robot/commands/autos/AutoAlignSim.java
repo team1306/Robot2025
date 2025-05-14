9668e5a0a1c698a7caf1047a911e04aaf6322c3a
ElectricTurtle32 <samuelnathanlewis@gmail.com>
2025-05-14 20:33:02 +0000

    Added limlight simulator

---
+++ src/main/java/frc/robot/commands/autos/AutoAlignSim.java
@@ -0,0 +1,90 @@
+
+package frc.robot.commands.autos;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+
+import frc.robot.util.LimelightHelpersSim;
+import frc.robot.subsystems.SwerveSubsystem;
+
+public class AutoAlignSim extends Command {
+  private PIDController xController, yController, rotationController;
+  private boolean direction;
+  private Timer noTagTimer, stopTimer;
+  private SwerveSubsystem drivebase;
+  private double tagID = -1;
+
+  public AutoAlignSim(boolean direction, SwerveSubsystem drivebase) {
+    xController = new PIDController(0, 0.0, 0);  
+    yController = new PIDController(0, 0.0, 0);  
+    rotationController = new PIDController(0, 0, 0);  
+    this.direction = direction; // 1 = right reef, 0 = left reef
+
+    this.drivebase = drivebase;
+    addRequirements(drivebase);
+  }
+
+  @Override
+  public void initialize() {
+    this.stopTimer = new Timer();
+    this.stopTimer.start();
+    
+    this.noTagTimer = new Timer();
+    this.noTagTimer.start();
+
+    rotationController.setSetpoint(Constants.ROT_SETPOINT);
+    rotationController.setTolerance(Constants.ROT_TOLERANCE);
+
+    xController.setSetpoint(Constants.X_SETPOINT);
+    xController.setTolerance(Constants.X_TOLERANCE);
+
+    yController.setSetpoint(direction ? Constants.Y_SETPOINT : -Constants.Y_SETPOINT);
+    yController.setTolerance(Constants.Y_TOLERANCE);
+
+    tagID = LimelightHelpersSim.getFiducialID();
+  }
+
+  @Override
+  public void execute() {
+    if (LimelightHelpersSim.getTV() && LimelightHelpersSim.getFiducialID() == tagID) {
+      this.noTagTimer.reset();
+
+      double[] postions = LimelightHelpersSim.getBotPose_TargetSpace(this.drivebase);
+
+      double xSpeed = xController.calculate(postions[2]);
+      double ySpeed = -yController.calculate(postions[0]);
+      double rotValue = -rotationController.calculate(postions[4]);
+
+      drivebase.drive(new ChassisSpeeds(xSpeed, ySpeed, rotValue));
+
+      if(
+        !rotationController.atSetpoint() ||
+        !yController.atSetpoint() ||
+        !xController.atSetpoint()
+        ) 
+      {
+        stopTimer.reset();
+      }
+    } else { 
+      drivebase.drive(new ChassisSpeeds(0, 0, 0));
+    }
+      
+    SmartDashboard.putNumber("poseValidTimer", stopTimer.get());
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    drivebase.drive(new ChassisSpeeds(0, 0, 0));
+  }
+
+  @Override
+  public boolean isFinished() {
+    return this.noTagTimer.hasElapsed(Constants.NO_TAG_WAIT_TIME) ||
+        stopTimer.hasElapsed(Constants.CORRECT_POSE_WAIT_TIME);
+  }
+}
---
+++ src/main/java/frc/robot/util/LimelightHelpersSim.java
@@ -0,0 +1,32 @@
+package frc.robot.util;
+import frc.robot.subsystems.SwerveSubsystem;
+
+import edu.wpi.first.networktables.DoubleArrayEntry;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.TimestampedDoubleArray;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Translation2d;
+
+public class LimelightHelpersSim {
+  
+   
+    public static double[] getBotPose_TargetSpace(SwerveSubsystem drivebase){
+        Pose2d pose = drivebase.getPose();
+        double[] poseArray = {5-pose.getY(), 0, 5-pose.getX(), 0, pose.getRotation().getDegrees(), 0};
+        return poseArray;
+    }
+    public static boolean getTV(){
+        return true;
+    }
+    public static double getFiducialID(){
+        return 21;
+    }
+
+}
