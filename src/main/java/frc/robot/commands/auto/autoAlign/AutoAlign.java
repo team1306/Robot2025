97fd302117ad2c5a382927faf50e14870ae12ba3
Connor Mulligan <cgmulligan33@gmail.com>
2025-09-03 21:35:13 -0500

    Changed auto align to be runnable

--- src/main/java/frc/robot/commands/auto/autoAlign/AutoAlign.java
+++
@@ -1,4 +0,0 @@
-package frc.robot.commands.auto.autoAlign;
-
-public interface AutoAlign {
-}
--- src/main/java/frc/robot/commands/auto/autoAlign/ComboAutoAlign.java
+++ src/main/java/frc/robot/commands/auto/autoAlign/ComboAutoAlign.java
@@ -4,20 +4,34 @@ import edu.wpi.first.wpilibj2.command.Command;
 
 public class ComboAutoAlign extends Command {
 
-    AutoAlign globalAutoAlign;
-    AutoAlign localAutoAlign;
+    private final Runnable globalAutoAlign;
+    private final Runnable localAutoAlign;
+
+    private Runnable currentAutoAlign;
+
+    public ComboAutoAlign(Runnable globalAutoAlign, Runnable localAutoAlign) {
+        this.globalAutoAlign = globalAutoAlign;
+        this.localAutoAlign = localAutoAlign;
+    }
 
     @Override
     public void initialize() {}
 
     @Override
     public void execute() {
+        if (true) { //TODO: change to use LL info
+            currentAutoAlign = globalAutoAlign;
+        } else {
+            currentAutoAlign = localAutoAlign;
+        }
 
+        currentAutoAlign.run();
     }
 
     @Override
     public boolean isFinished() {return true;}
 
     @Override
-    public void end(boolean interrupted) {}
+    public void end(boolean interrupted) {
+    }
 }
