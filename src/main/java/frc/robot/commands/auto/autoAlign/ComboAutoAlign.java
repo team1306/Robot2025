e40f6daf3c0b3dceffee4247f58a3d5b87a7700d
Connor Mulligan <cgmulligan33@gmail.com>
2025-09-09 20:24:17 -0500

    created decision loop

--- src/main/java/frc/robot/commands/auto/autoAlign/ComboAutoAlign.java
+++ src/main/java/frc/robot/commands/auto/autoAlign/ComboAutoAlign.java
@@ -2,17 +2,41 @@ package frc.robot.commands.auto.autoAlign;
 
 import edu.wpi.first.wpilibj2.command.Command;
 
+import java.util.Arrays;
+import java.util.List;
+import frc.robot.Constants;
+import frc.robot.util.LimelightHelpers;
+
 public class ComboAutoAlign extends Command {
 
-    AutoAlign globalAutoAlign;
-    AutoAlign localAutoAlign;
+    List<Integer> validTags = Arrays.asList(
+        17, 18, 19, 20, 21, 22, //blue
+        6, 7, 8, 9, 10, 11 //red
+    );
+
+    Command globalAutoAlign;
+    Command localAutoAlign;
+
+    public ComboAutoAlign(Command globalAutoAlign, Command localAutoAlign) {
+
+    }
 
     @Override
-    public void initialize() {}
+    public void initialize() {
+    }
 
     @Override
     public void execute() {
+        boolean targetVisible = LimelightHelpers.getTV(Constants.LIMELIGHT_4_NAME);
+        double tagId = LimelightHelpers.getFiducialID(Constants.LIMELIGHT_4_NAME);
 
+        if(targetVisible && validTags.contains((int)tagId)) { //can see valid april tag
+            globalAutoAlign.cancel();
+            localAutoAlign.schedule();
+        } else {
+            localAutoAlign.cancel();
+            globalAutoAlign.schedule();
+        }
     }
 
     @Override
